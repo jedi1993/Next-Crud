1d6f44c71eb8fdc92c41c1eb02f84b1748176d53
Henrique Oliveira <henri.oliveira.93@gmail.com>
2023-01-23 15:31:41 -0300

    subindo projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return(
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white`}>
+        <span>Texto</span>
+      </div>
+  )
+}
